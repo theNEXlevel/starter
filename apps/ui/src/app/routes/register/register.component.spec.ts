dcf8d76eeae465ec02e147049b2cb4be14050ced
Justin McLellan <justin.mclellan99@gmail.com>
2022-08-10 17:28:10 -0500

    initial register unit testing - needs subscribe tests working

--- apps/ui/src/app/routes/register/register.component.spec.ts
+++ apps/ui/src/app/routes/register/register.component.spec.ts
@@ -1,8 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { AbstractControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { EMPTY, throwError } from 'rxjs';
 import { AuthService } from '../../shared/services/auth.service';
 
 import { RegisterComponent } from './register.component';
@@ -13,21 +14,13 @@ const routerMock = {
   navigate: jest.fn(),
 };
 
-const fbMock = {
-  group: jest.fn().mockReturnValue({
-    invalid: false,
-    get: jest.fn().mockReturnValue('123'),
-    email: '123',
-    password: '123',
-  }),
-};
-
 const mockMsgSvc = {
   add: jest.fn(),
 };
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
+  let authService: AuthService;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
@@ -35,7 +28,7 @@ describe('RegisterComponent', () => {
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [RegisterComponent],
       providers: [
-        { provide: NonNullableFormBuilder, useValue: fbMock },
+        NonNullableFormBuilder,
         AuthService,
         { provide: Router, useValue: routerMock },
         { provide: MessageService, useValue: mockMsgSvc },
@@ -43,11 +36,104 @@ describe('RegisterComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
+    authService = TestBed.inject(AuthService);
     component = fixture.componentInstance;
+    authService.register = jest.fn().mockReturnValue(EMPTY);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should set loadingSubject to a behaviorSubject and set it to false', () => {
+      component.ngOnInit();
+      expect(component.loadingSubject.value).toEqual(false);
+    });
+    it('should set loading$ to an observable', () => {
+      component.ngOnInit();
+      expect(typeof component.loading$).toEqual(typeof component.loadingSubject.asObservable());
+    });
+    it('should set form to a formGroup with 2 input fields defaulted to empty strings', () => {
+      component.ngOnInit();
+      expect(component.form.get('email')?.value).toEqual('');
+      expect(component.form.get('password')?.value).toEqual('');
+    });
+  });
+
+  describe('submit', () => {
+    it('should not call markAsDirty if the fields do not exist', () => {
+      component.form = new FormGroup({});
+      expect(component.submit()).toEqual(undefined);
+    });
+    it('should call markAsDirty on the password and email form fields', () => {
+      (component.email as AbstractControl).markAsDirty = jest.fn();
+      (component.password as AbstractControl).markAsDirty = jest.fn();
+      component.submit();
+      expect(component.email?.markAsDirty).toHaveBeenCalledTimes(1);
+      expect(component.password?.markAsDirty).toHaveBeenCalledTimes(1);
+    });
+    it('should call next on loadingSubject with true', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.loadingSubject.next = jest.fn();
+      component.submit();
+      expect(component.loadingSubject.next).toHaveBeenCalledTimes(1);
+      expect(component.loadingSubject.next).toHaveBeenCalledWith(true);
+    });
+    it('should call register of authSvc with form value', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.submit();
+      expect(authService.register).toHaveBeenCalledTimes(1);
+      expect(authService.register).toHaveBeenCalledWith(component.form.value);
+    });
+    it('should call add on messageSvc with params on success', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.submit();
+      fixture.detectChanges();
+      authService.register(component.form.value).subscribe(() => {
+        expect(mockMsgSvc.add).toHaveBeenCalledTimes(1);
+        expect(mockMsgSvc.add).toHaveBeenCalledWith({
+          severity: 'success',
+          summary: 'Registered',
+          detail: 'You have been logged in!',
+        });
+      });
+    });
+    it('should call next on loadingSubject with false value on success', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.loadingSubject.next = jest.fn();
+      component.submit();
+      authService.register(component.form.value).subscribe(() => {
+        expect(component.loadingSubject.next).toHaveBeenCalledTimes(1);
+        expect(component.loadingSubject.next).toHaveBeenCalledWith(false);
+      });
+    });
+    it('should call navigate on router with dashboard on success', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.submit();
+      authService.register(component.form.value).subscribe(() => {
+        expect(routerMock.navigate).toHaveBeenCalledTimes(1);
+        expect(routerMock.navigate).toHaveBeenCalledWith(['dashboard']);
+      });
+    });
+    it('should call next on loadingSubject with false value on error', () => {
+      component.email?.setValue('test@test.com');
+      component.password?.setValue('123');
+      component.loadingSubject.next = jest.fn();
+      authService.register = jest.fn().mockReturnValue(throwError(() => ''));
+      component.submit();
+      authService.register(component.form.value).subscribe({
+        error: () => {
+          expect(component.loadingSubject.next).toHaveBeenCalledTimes(2);
+          expect(component.loadingSubject.next).toHaveBeenNthCalledWith(2, false);
+        },
+      });
+    });
   });
 });
--- apps/ui/src/app/routes/register/register.component.ts
+++ apps/ui/src/app/routes/register/register.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Login } from '@starter/api-interfaces';
 import { MessageService } from 'primeng/api';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from '../../shared/services/auth.service';
+
+export interface LoginForm {
+  email: AbstractControl<string>;
+  password: AbstractControl<string>;
+}
 
 @Component({
   selector: 'starter-register',
@@ -12,28 +17,28 @@ import { AuthService } from '../../shared/services/auth.service';
   styleUrls: ['./register.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent {
-  loadingSubject = new BehaviorSubject<boolean>(false);
-  loading$ = this.loadingSubject.asObservable();
-
-  form = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
+export class RegisterComponent implements OnInit {
+  loadingSubject!: BehaviorSubject<boolean>;
+  loading$!: Observable<boolean>;
+  form!: FormGroup;
 
   get email() {
     return this.form.get('email');
   }
-
   get password() {
     return this.form.get('password');
   }
-  constructor(
-    private fb: NonNullableFormBuilder,
-    private authSvc: AuthService,
-    private router: Router,
-    private messageSvc: MessageService
-  ) {}
+
+  constructor(private authSvc: AuthService, private router: Router, private messageSvc: MessageService) {}
+
+  ngOnInit(): void {
+    this.loadingSubject = new BehaviorSubject<boolean>(false);
+    this.loading$ = this.loadingSubject.asObservable();
+    this.form = new FormGroup<LoginForm>({
+      email: new FormControl<string>('', { validators: [Validators.required, Validators.email], nonNullable: true }),
+      password: new FormControl<string>('', { validators: [Validators.required], nonNullable: true }),
+    });
+  }
 
   submit(): void {
     this.email?.markAsDirty();
