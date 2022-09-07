181e4d4aef3037ad135b29f64202901aec72857a
Justin McLellan <justin.mclellan99@gmail.com>
2022-09-06 21:06:03 -0500

    add redux devtools

--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActionReducer, ActionReducerMap, MetaReducer, StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { localStorageSync } from 'ngrx-store-localstorage';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { userReducer, msgReducer, AppState, UserEffects } from './state';
+import { environment } from '../environments/environment';
 
 const reducers: ActionReducerMap<AppState> = { user: userReducer, msg: msgReducer };
 
@@ -37,6 +39,11 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     HttpClientModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([UserEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   bootstrap: [AppComponent],
 })
