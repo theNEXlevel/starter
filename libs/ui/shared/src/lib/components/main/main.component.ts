2c5a75d10c622f037b7269c3dd912accb4272cbb
Justin McLellan <justin.mclellan99@gmail.com>
2022-12-09 22:49:47 -0600

    Update material

--- libs/ui/shared/src/lib/components/main/main.component.ts
+++ libs/ui/shared/src/lib/components/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy, OnInit, inject } from '@angular/core';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
 import { filter, tap } from 'rxjs';
 import { selectMsg } from '../../state';
