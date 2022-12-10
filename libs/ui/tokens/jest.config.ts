f81f665e3ffb1629b4ea8a78d07fdfbd442eb54a
Justin McLellan <justin.mclellan99@gmail.com>
2022-12-09 22:52:38 -0600

    fix jest config

--- libs/ui/tokens/jest.config.ts
+++ libs/ui/tokens/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'ui-tokens-tokens',
-  preset: '../../../../jest.preset.js',
+  displayName: 'ui-tokens',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/ui/tokens',
+  coverageDirectory: '../../../coverage/libs/ui/tokens',
 };
